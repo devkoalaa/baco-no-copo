c7c0e9565f2883e09c3f96e12a76158d06f9d91b
Afrontoso <victor2lra@gmail.com>
2023-07-17 22:55:27 -0300

    criado lista de itens com contador

--- App.tsx
+++ App.tsx
@@ -3,30 +3,63 @@ import { Input, TamaguiProvider, Theme, XStack, YStack, Text } from "tamagui";
 import { ChangeTheme } from "./src/components/ChangeTheme";
 import { User } from "./src/components/User";
 import config from "./tamagui.config";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "./src/components/Button";
+import * as SecureStore from 'expo-secure-store'
+
+interface Item {
+  nome: string;
+  quantidade: number;
+}
 
 export default function App() {
+  useEffect(() => {
+    SecureStore.getItemAsync('BacoNoCopo.itens').then((response) => {
+      const resultado = response
+      resultado && setListaItens(JSON.parse(resultado))
+    })
+  }, [])
+
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const [loaded] = useFonts({
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
   });
 
-  const [itemValue, setItemValue] = useState("");
-  const [listaItens, setListaItens] = useState<string[]>([]);
+  const [addItem, setAddItem] = useState("")
+  const [listaItens, setListaItens] = useState<Item[]>([]);
+
+  useEffect(() => {
+    SecureStore.setItemAsync('BacoNoCopo.itens', JSON.stringify(listaItens))
+  }, [listaItens])
 
   if (!loaded) {
     return null;
   }
 
-  function submit() {
-    console.log("item:", itemValue);
+  function submitItem() {
+    if (addItem) {
+      const newItem: Item = { nome: addItem, quantidade: 0 };
 
-    setListaItens((e) => [...e, itemValue]);
-    console.log("lista:", listaItens);
+      setListaItens([newItem, ...listaItens])
 
-    console.log("Entrou no submit()");
+      console.log("item:", addItem);
+      console.log("lista:", listaItens);
+
+      setAddItem("")
+    }
+  }
+
+  function somaItem(selectedItem: Item) {
+
+    const listaUpdate = listaItens.map(obj => {
+
+      if (obj.nome == selectedItem.nome) {
+        obj.quantidade++;
+      }
+      return obj;
+    })
+    setListaItens(listaUpdate)
   }
 
   return (
@@ -42,17 +75,24 @@ export default function App() {
               f={1}
               w="$5"
               h="$5"
-              value={itemValue}
-              onChangeText={setItemValue}
+              value={addItem}
+              onChangeText={setAddItem}
               placeholder="Adicionar item..."
               focusStyle={{ bw: 2, boc: "$blue10" }}
             />
-            <Button tipo="normal" onPress={submit} />
+            <Button tipo="normal" onPress={submitItem} />
           </XStack>
-          {listaItens.map((item, index) => (
-            <Text key={index} m="$1" backgroundColor='$gray10' borderRadius='$2'>
-              {item}
-            </Text>
+          {listaItens && listaItens.map((item: Item, index: number) => (
+            <YStack onPress={(e) => somaItem(item)} key={index} p="$4" marginVertical="$2" backgroundColor='$gray10Light' borderRadius='$2'>
+              <XStack justifyContent="space-between">
+                <Text>
+                  {item.nome}
+                </Text>
+                <Text fontWeight="bold">
+                  {item.quantidade}
+                </Text>
+              </XStack>
+            </YStack>
           ))}
         </YStack>
       </Theme>
